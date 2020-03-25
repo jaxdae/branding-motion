3ac1d2f214d510f90bac6187a7f8f3a15236bfd4
Jacqueline Dähn <jacqueline.daehn@towa.at>
2020-03-25 23:16:40 +0100

    heroku

--- server/config/config.js
+++ server/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: be6c7309de8f03,
+    password: f0013e25,
+    database: heroku_07e5ad34e269a7f,
+    host: eu - cdbr - west - 02.cleardb.net,
     dialect: 'mysql'
   }
 }
